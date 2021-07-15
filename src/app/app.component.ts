129b75f05b052900c5085ac511c5199e6af9d856
Zongi93 <dearon@indamail.hu>
2021-07-15 18:01:41 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { combineLatest,map,BehaviorSubject} from 'rxjs';
 import { Product } from './model';
 import { DhlService } from './services/dhl.service';
 import { StoreService } from './services/store.service';
@@ -9,7 +10,8 @@ import { StoreService } from './services/store.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  readonly inventory$ = this.storeService.inventory$;
+  private readonly filterByTextSubject = new BehaviorSubject<string>('');
+  readonly inventory$ = combineLatest(this.storeService.inventory$, this.filterByTextSubject).pipe(map(this.filterInventoryByInput));
 
   constructor(
     private readonly storeService: StoreService,
@@ -20,5 +22,15 @@ export class AppComponent {
 
   order(product: Product) {
     this.storeService.order(product);
+  }
+
+  onFilterKeyDown(event: KeyboardEvent): void {
+    // event.target.value should be defined but for somereason stackblitz doesnt know that
+    const inputText = (event.target as any).value ?? (document.getElementById('input') as HTMLInputElement).value;
+    this.filterByTextSubject.next(inputText);
+  }
+
+  private filterInventoryByInput([inventory, filterBy]: [Array<Product>, string]): Array<Product> {
+    return inventory.filter(product => filterBy.length === 0 || product.name.toLocaleLowerCase().indexOf(filterBy.toLocaleLowerCase()) > -1);
   }
 }
